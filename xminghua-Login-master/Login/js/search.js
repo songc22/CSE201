6e38e690c896c7301e90055a3b8e74dee38677a9
songc22 <52966044+songc22@users.noreply.github.com>
2019-08-07 19:41:40 -0400

    Add files via upload

---
+++ xminghua-Login-master/Login/js/search.js
@@ -0,0 +1,9 @@
+function search(){
+    if(value == '2'){
+        document.write(Date());
+    }
+    if(value=='1'){
+        $('#Cont_2').removeClass('hide');
+        $('#Cont_1,#Cont_3').addClass('hide');
+    }
+}
